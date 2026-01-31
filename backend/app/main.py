426eaaffaa65b407873571a0cb825e66495b7713
Nguyen Huu Hanh Lam <nguyenhuuhanhlam@gmail.com>
2026-01-31 10:19:21 +0700

    ...

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 from scalar_fastapi import get_scalar_api_reference
 
-
-# from app.core.config import settings
 from app.api.router import api_router
 
 app = FastAPI(
@@ -16,12 +14,6 @@ app.include_router(api_router)
 
 @app.get("/docs", include_in_schema=False)
 def scalar_docs():
-    """
-    Serve Scalar API documentation UI.
-
-    - openapi_url: Path to the OpenAPI schema
-    - title: Title displayed in Scalar UI
-    """
     return get_scalar_api_reference(
         openapi_url="/openapi.json",
         title="LIMS API Documentation",
